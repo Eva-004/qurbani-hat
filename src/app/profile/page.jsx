9adeb824ba02bbca2746cfb7b39b128dc96b7134
Eva-004 <foredajannateva@gmail.com>
2026-05-01 20:14:17 +0600

    Added login , register authentication and added profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,23 @@
+'use client'
+import UpdateProfile from '@/components/homePage/UpdateProfile';
+import { authClient } from '@/lib/auth-client';
+import { Avatar, Card } from '@heroui/react';
 import React from 'react';
 
 const MyProfilePage = () => {
+    const userData = authClient.useSession();
+    const user = userData.data?.user;
     return (
-        <div>
-            profile page
+        <div className='mt-10'>
+            <Card className='max-w-96 mx-auto flex flex-col  items-center border'>
+                <Avatar className='w-40 h-40'>
+                    <Avatar.Image alt={user?.name} src={user?.image} referrerPolicy='no-referrer' className='w-full object-cover' />
+                    <Avatar.Fallback>{user?.name[0]}</Avatar.Fallback>
+                </Avatar>
+               <h2 className='font-bold text-xl'>{user?.name}</h2>
+               <p className='text-gray-600'>{user?.email}</p>
+               <UpdateProfile></UpdateProfile>
+            </Card>
         </div>
     );
 };
