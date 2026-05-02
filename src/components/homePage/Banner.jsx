897578a1f10fc9e5c6b56eab1d7d070538c64c52
Eva-004 <foredajannateva@gmail.com>
2026-05-02 23:38:57 +0600

    Fix responsive style

--- src/components/homePage/Banner.jsx
+++ src/components/homePage/Banner.jsx
@@ -16,10 +16,10 @@ const Banner = () => {
         <div  className="mt-6 bg-[url('https://static.vecteezy.com/system/resources/previews/024/230/442/non_2x/islamic-background-for-eid-al-adha-with-mosque-mandala-cow-and-goat-icons-banner-template-with-empty-space-for-text-qurban-day-illustration-free-vector.jpg')] h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl">
 
 
-            <div className=' mx-auto space-y-2 text-center'>
+            <div className=' md:mx-auto space-y-2 text-center'>
                 <animated.h2 style={props} className='font-bold text-4xl md:text-5xl bg-gradient-to-r from-blue-800 to-purple-500 bg-clip-text text-transparent'>QurbaniHat</animated.h2>
-                <h2 className='font-bold text-xl md:text-5xl  '> Livestock Booking Platform</h2>
-                <p className=' text-sm md:text-lg w-100 mx-auto'>Explore verified cows and goats for Qurbani with a smooth and secure booking experience.</p>
+                <h2 className='font-bold text-2xl md:text-5xl  '> Livestock Booking Platform</h2>
+                <p className=' text-lg md:w-100 md:mx-auto'>Explore verified cows and goats for Qurbani with a smooth and secure booking experience.</p>
                 <button className='btn  btn-primary text-xl mt-2 animate-animated animate-bounce'><Link href={'/all-animals'} className='flex gap-2 items-center  text-white '>Browse Animals <FiArrowUpRight /></Link></button>
             </div>
         </div>
